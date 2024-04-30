6fe6ccdc1cedc306bb68532401e9ca2ea9aec1bc
Burair010 <168530913+Burair010@users.noreply.github.com>
2024-04-30 14:08:05 +0500

    Update NUL.py

--- NUL.py
+++ NUL.py
@@ -1,1 +1,11 @@
-
+Firstpy = "nose"
+print(Firstpy)
+renderfirstableto = True
+print(str(renderfirstableto))
+# this is used to be check basic render i know this is this is trash but it's a start
+array = " "
+print(array)
+array = "hi"
+print(array)
+array = ":)"
+print(array)
